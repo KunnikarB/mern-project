2318b20cd9f81944707e6fb8e0df142eb345c8a5
kunnikarB <kunnikarboon@gmail.com>
2025-10-14 16:19:12 +0200

    update client API

--- server/index.js
+++ server/index.js
@@ -9,9 +9,23 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://mern-deploy-client-psi.vercel.app/',
+];
+
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`CORS policy: ${origin} not allowed`));
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
